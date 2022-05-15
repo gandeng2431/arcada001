7f5a508a503ee6b1497aed21e24bc085c57c9804
gandeng2431 <103360522+gandeng2431@users.noreply.github.com>
2022-05-15 12:30:48 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,3 +1,32 @@
+from random import randit
+from pygame import*
+font.init()
+font=font.Font(None,72)
+#mixer.init()
+#mixer.init.load('')
+#mixer.musik.play()
+win_width=800
+win_height=600
+left_bound=win_width / 40
+right_bound=win_height - 8 * left_bound
+shift=0
+x_start=20
+y_start=10
+img_file_back='bg.jpg'
+img_file_hero='player.png'
+img_file_enemy='sprite.png'
+img_file_bomb='bomb.png'
+img_file_door='dorr.png'
+img_wall='wall.png'
+FPS=60
+
+C_WHITE=(255,255,255)
+C_DARK=(48,48,0)
+C_YELLOW=(255,255,87)
+C_GREEN=(32,128,32)
+C_RED=(255,0,0)
+C_BLACK=(0,0,0,)
+
 #final sprite
 class FinalSprite(sprite.Sprite):
     def __init__(self, player_image, player_x , player_y, player_speed):
