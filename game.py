cbfbe44c7f20b0df68f58ae80789c1fad385e9b0
MakaGT <103880074+MakaGT@users.noreply.github.com>
2022-05-15 12:29:54 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,1 +1,11 @@
+#final sprite
+class FinalSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x , player_y, player_speed):
+        sprite.Sprite.__init__(self)
 
+        self.image = transform.scale(image.load(player_image) , (100 ,100))
+        self.speed = player_speed
+
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
